ad6c39e39032a8fd43aa9928596c516d014c4bb7
nelay-tw <nelay.karmakar@techwens.in>
2024-08-29 17:57:53 +0530

    add category filter

--- app/routes/expense_routes.py
+++ app/routes/expense_routes.py
@@ -1,18 +1,20 @@
 from app.db.connection import mongodb
-from app.controller.expense_controller import entry_expense
+from app.controller.expense_controller import entry_expense, upload_file, filter_sms_category, get_all_data
 from app.models.expense_model import ExpenseRequest
-from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi import APIRouter, File, UploadFile, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pathlib import Path
 from app.core.config import Settings
 import os
+from typing import List, Dict
+
 
 
 router = APIRouter()
 
 @router.get("/")
 async def read_root():
-    return {"message": "Welcome to the Expense Tracker API"}
+    return {"Welcom message": "Welcome to the Expense Tracker API"}
 
 @router.post("/expense_detail")
 async def expense_detail(expense_request: ExpenseRequest):
@@ -27,42 +29,33 @@ async def expense_detail(expense_request: ExpenseRequest):
 
 
 
-# Define the upload directory
-UPLOAD_DIR = Path(Settings.UPLOAD_DIR)
 
-# Ensure the upload directory exists
-UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
 @router.post("/upload_image")
 async def upload_image(image: UploadFile = File(...)):
     if not image.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="Invalid image format")
 
-    # Define the file path 
-    file_path = UPLOAD_DIR / image.filename
+    try:
+        result = await upload_file(image)
+        return {"result": result.body}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
 
-    # Save the image in upload file
-    with open(file_path, "wb") as buffer:
-        buffer.write(await image.read())
 
-    # Store the image path in MongoDB
-    db = await mongodb.get_database()
-    collection = db["images"]  # Use the "images" collection
+@router.get("/filter_sms_category/")
+async def filter_sms(categories: List[str] = Query(...)):
+    
+    result = await filter_sms_category(categories)
+    return {"Entered Categories":categories, "Filtered Data": result}
 
-    base_url = "http://127.0.0.1:8000/"
-    document = {
-        "image_filename": image.filename,
-        "file_path": base_url + str(file_path.relative_to("app")),
-        "content_type": image.content_type
-    }
 
-    insert_result = await collection.insert_one(document)
 
-    return JSONResponse(
-        status_code=200,
-        content={
-            "filename": image.filename,
-            "filepath": base_url + str(file_path.relative_to("app")),
-            "inserted_id": str(insert_result.inserted_id)
-        }
-    )
+
+@router.get("/all_sms_data")
+async def get_data():
+    data = await get_all_data()
+    return {"All Data": data}
+    
