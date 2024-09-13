5714ea47512604e53aa771c9f5ca392d86ee0263
Rahul <sabuirahul90@gmail.com>
2024-09-13 18:44:39 +0530

    working on code modification

--- app/core/config.py
+++ app/core/config.py
@@ -4,9 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    HOST: str = os.getenv("HOST", "0.0.0.0")
-    PORT: int = int(os.getenv("PORT", "8000"))
+    HOST: str = os.getenv("HOST")
+    PORT: int = int(os.getenv("PORT"))
     
-    MONGO_URL: str = os.getenv("MONGO_URL", "mongodb+srv://nelaykarmakar:0u7rxkmMOxmXJFjj@expensetracker.i0cqo.mongodb.net/expense")  # Set a default for local testing
-
+    MONGO_URL: str = os.getenv("MONGO_URL")
 settings = Settings()
