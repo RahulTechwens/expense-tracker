948aa8fab88ed660eea35c28562ceeb9ea7f6a5a
Rahul <sabuirahul90@gmail.com>
2024-09-13 18:35:34 +0530

    working on code modification

--- app/main.py
+++ app/main.py
@@ -8,8 +8,17 @@ from app.routes.alert_routes import router as alert_router
 from app.routes.parse_sms_routes import router as parse_sms_router
 import uvicorn # type: ignore
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this to restrict allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Update this to restrict allowed methods
+    allow_headers=["*"],  # Update this to restrict allowed headers
+)
 
 # app.mount("/upload", StaticFiles(directory=settings.UPLOAD_DIR), name="upload")
 
