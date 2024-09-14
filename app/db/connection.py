1d0c11906ea6b56cbc0bc8137d1fba57cd487e96
Rahul <sabuirahul90@gmail.com>
2024-09-14 09:52:06 +0530

    working on code modification

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,14 +1,10 @@
-# from motor.motor_asyncio import AsyncIOMotorClient # type: ignore
 from mongoengine import connect
 from app.core.config import settings
 class MongoDB:
     def __init__(self, uri: str, db_name: str):
-    # Define the default connection using mongoengine
         connect(db_name, host=uri)
-        print(connect(db_name, host=uri))
 
     async def get_database(self):
-        print(self.db)
         return self.db
 
 mongodb = MongoDB(settings.MONGO_URL, "expenses")
--- app/main.py
+++ app/main.py
@@ -1,27 +1,12 @@
-# import os
-# os.environ["PYTHONDONTWRITEBYTECODE"] = 1
-
 from fastapi import FastAPI # type: ignore
-from fastapi.staticfiles import StaticFiles # type: ignore
 from app.routes.expense_routes import router as expense_router
 from app.routes.alert_routes import router as alert_router
 from app.routes.parse_sms_routes import router as parse_sms_router
 import uvicorn # type: ignore
 from app.core.config import settings
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Update this to restrict allowed origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Update this to restrict allowed methods
-    allow_headers=["*"],  # Update this to restrict allowed headers
-)
-
-print("main")
-# app.mount("/upload", StaticFiles(directory=settings.UPLOAD_DIR), name="upload")
 
 app.include_router(expense_router, prefix="/api", tags=["Expenses"])
 app.include_router(alert_router, prefix="/api", tags=["Alerts"])
